66ecdafb0e54c4334cf2442a175894c327627647
Dino Fejzagić <dino.f@live.de>
2024-05-08 08:36:31 +0200

    Update manifest

---
+++ Runtime/Input/Controllers/OpenXRControllerServiceModuleProfile.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using RealityToolkit.Definitions.Controllers;
+
+namespace RealityToolkit.OpenXR.Input.Controllers
+{
+    /// <summary>
+    /// Configuration profile for <see cref="OpenXRControllerServiceModule"/>.
+    /// </summary>
+    public class OpenXRControllerServiceModuleProfile : BaseControllerServiceModuleProfile
+    {
+        /// <inheritdoc />
+        public override ControllerDefinition[] GetDefaultControllerOptions()
+        {
+            return null;
+        }
+    }
+}
